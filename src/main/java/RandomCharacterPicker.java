609ffc76f444c48c4d5223cc8589997e004e9291
Dawid Rybak <d.rybak.2001@gmail.com>
2021-06-06 18:58:29 +0200

    Add case for random character pick

--- src/main/java/RandomCharacterPicker.java
+++ src/main/java/RandomCharacterPicker.java
@@ -1,12 +1,17 @@
 import java.util.List;
+import java.util.Random;
 
 public class RandomCharacterPicker {
+    private final Random random = new Random();
+
     public String pick(List<String> issacCharacters) {
 
-        if (issacCharacters == null || issacCharacters.isEmpty())
-         throw new IllegalArgumentException();
-        else
-            return issacCharacters.get(0);
+        if (issacCharacters == null || issacCharacters.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        int randomInt = random.nextInt(issacCharacters.size());
+        return issacCharacters.get(randomInt);
     }
 
 }
--- src/test/java/RandomCharacterPickerTest.java
+++ src/test/java/RandomCharacterPickerTest.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,5 +38,29 @@ class RandomCharacterPickerTest {
 
         // THEN
         Assertions.assertEquals(onlyCharacterInCollection, result);
+    }
+
+    @Test
+    void shouldReturnRandomCharacterForMultipleElementList() {
+        // GIVEN
+        int randomGenerations = 1000;
+        List<String> characterPool = Arrays.asList("Isaac", "Eden", "Cain");
+        int expectedProbability = randomGenerations / characterPool.size();
+        RandomCharacterPicker randomCharacterPicker = new RandomCharacterPicker();
+        List<String> pickingResults = new ArrayList<>();
+        // WHEN
+
+        for (int i = 0; i < randomGenerations; i++) {
+            randomCharacterPicker.pick(characterPool);
+            pickingResults.add(randomCharacterPicker.pick(characterPool));
+        }
+
+        // THEN
+        Map<String, Long> charactersOccurrences = pickingResults.stream()
+                                                                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (Long occurrence : charactersOccurrences.values()) {
+            Assertions.assertEquals(expectedProbability, occurrence, 40);
+
+        }
     }
 }
